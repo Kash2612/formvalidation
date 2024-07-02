6bc6c415f6436a1ee6302f380b4c1bfe193accef
Kashish Gupta <kashgupta20@gmail.com>
2024-07-03 00:08:32 +0530

    validation using regex updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Form from './Form';
+import Details from './Details';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>React Form Validation</h1>
+        </header>
+        <Routes>
+          <Route exact path="/" element={<Form />} />
+          <Route path="/details" element={<Details />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
